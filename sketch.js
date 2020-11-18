f60cddc90f3712d0e641150c88ea29f3030953d5
ARJUN2009 <68690799+ARJUN-ILANGO@users.noreply.github.com>
2020-11-18 15:31:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function preload(){
   player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
     
   
-  bananaImage = loadImage("Banana.png");
+  bananaImage = loadImage("banana.png");
   obstacle_img = loadImage("stone.png"); 
   
 }
